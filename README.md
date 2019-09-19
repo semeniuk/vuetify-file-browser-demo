addb65bf7dd8b3e79a83d32f6aa623e167c717ee
Yurii Semeniuk <mail@ysemeniuk.com>
2019-09-19 20:32:10 +0300

    minor changes

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 # Vuetify File Browser Demo
 
-[Demo](https://vuetify-file-browser-demo.herokuapp.com/)
+Demo of [Vuetify File Browser](https://github.com/semeniuk/vuetify-file-browser)
+
+[💣 Demo](https://vuetify-file-browser-demo.herokuapp.com/) 
+
+[🚀 Backend](https://github.com/semeniuk/vuetify-file-browser-server)
+
+![Screenshot](https://user-images.githubusercontent.com/15949274/65264191-c6a55c00-db16-11e9-841a-81e3906e5ca7.PNG)
 
 ## Project setup
 ```
--- src/App.vue
+++ src/App.vue
@@ -11,12 +11,12 @@
             <v-app-bar-nav-icon @click.stop="drawer = !drawer" class="mr-0"></v-app-bar-nav-icon>
             <v-toolbar-title>
                 <v-btn text to="/">
-                    <v-icon class="mr-2">mdi-view-dashboard</v-icon>
+                    <v-icon class="mr-2">mdi-folder-multiple-outline</v-icon>
                     <span class="title">Vuetify File Browser</span>
                 </v-btn>
             </v-toolbar-title>
             <div class="flex-grow-1"></div>
-            <v-btn icon>
+            <v-btn icon href="https://github.com/semeniuk/vuetify-file-browser" target="_blank">
                 <v-icon style="font-size: 32px" color="blue-grey darken-4">mdi-github-circle</v-icon>
             </v-btn>
         </v-app-bar>
